2e3716e9b010d5f0dd7d25d6c2b70ce31ba60009
Ayman Physics <mr_aymans@hotmail.com>
2022-11-05 03:20:32 +0200

    0

---
+++ Viewer/Viewer.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Viewer
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.BackColor = System.Drawing.Color.Black
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.SplitContainer1.Panel1.Controls.Add(Me.PictureBox1)
+        Me.SplitContainer1.Panel1.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.SplitContainer1.Panel2.Controls.Add(Me.PictureBox2)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.Button1)
+        Me.SplitContainer1.Panel2.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.SplitContainer1.Size = New System.Drawing.Size(1097, 265)
+        Me.SplitContainer1.SplitterDistance = 539
+        Me.SplitContainer1.SplitterWidth = 1
+        Me.SplitContainer1.TabIndex = 0
+        Me.SplitContainer1.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(539, 265)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Dock = System.Windows.Forms.DockStyle.Right
+        Me.PictureBox2.Location = New System.Drawing.Point(18, 0)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(539, 265)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Button1.Location = New System.Drawing.Point(30, 46)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(0, 0)
+        Me.Button1.TabIndex = 0
+        Me.Button1.TabStop = False
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Viewer
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.Button1
+        Me.ClientSize = New System.Drawing.Size(1097, 265)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Viewer"
+        Me.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Viewer"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        Me.SplitContainer1.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents SplitContainer1 As System.Windows.Forms.SplitContainer
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ Viewer/Viewer.vb
@@ -0,0 +1,58 @@
+﻿Public Class Viewer
+
+    Dim StartIndex As Integer = 1
+    Dim EndIndex As Integer = 673
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        StartIndex = GetSetting(Application.ProductName, Name, "StartIndex", StartIndex)
+        LoadImages(StartIndex)
+
+    End Sub
+
+    Sub LoadImages(ByVal i As Integer)
+        Try
+            'SplitContainer1.Panel1.BackgroundImage = Image.FromFile(Application.StartupPath & "\data\Quran_Page_" & i.ToString.PadLeft(3, "0") & ".fbk")
+            'SplitContainer1.Panel2.BackgroundImage = Image.FromFile(Application.StartupPath & "\data\Quran_Page_" & (i + 1).ToString.PadLeft(3, "0") & ".fbk")
+            PictureBox1.Image = Image.FromFile(Application.StartupPath & "\data\Quran_Page_" & i.ToString.PadLeft(3, "0") & ".fbk")
+            PictureBox2.Image = Image.FromFile(Application.StartupPath & "\data\Quran_Page_" & (i + 1).ToString.PadLeft(3, "0") & ".fbk")
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Sub Form1_KeyDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyUp, SplitContainer1.KeyDown
+        If (e.KeyData = Keys.Up Or e.KeyData = Keys.Right) And StartIndex > 1 Then
+            StartIndex -= 2
+        ElseIf (e.KeyData = Keys.Down Or e.KeyData = Keys.Left) And StartIndex < EndIndex Then
+            StartIndex += 2
+        ElseIf e.KeyData = Keys.Home Then
+            StartIndex = 1
+        ElseIf e.KeyData = Keys.End Then
+            StartIndex = EndIndex
+        ElseIf e.KeyData = Keys.PageUp And StartIndex > 1 Then
+            StartIndex -= 20
+            If StartIndex < 1 Then StartIndex = 1
+        ElseIf e.KeyData = Keys.PageDown And StartIndex < EndIndex Then
+            StartIndex += 20
+            If StartIndex > EndIndex Then StartIndex = EndIndex
+        Else
+            Return
+        End If
+        LoadImages(StartIndex)
+        SaveSetting(Application.ProductName, Name, "StartIndex", StartIndex)
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Close()
+    End Sub
+
+    'Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+    '    StartIndex += 2
+    '    LoadImages(StartIndex)
+    '    SaveSetting(Application.ProductName, Name, "StartIndex", StartIndex)
+    'End Sub
+
+    'Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+    '    StartIndex -= 2
+    '    LoadImages(StartIndex)
+    '    SaveSetting(Application.ProductName, Name, "StartIndex", StartIndex)
+    'End Sub
+End Class
